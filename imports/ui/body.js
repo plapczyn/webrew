8cf5009700686026ca3f1106d82f5fbdd70b6c44
Philip Lapczynski <philip.lapczynski@gmail.com>
2017-01-25 23:53:59 -0500

    added responsive coffee bean hero background

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -2,3 +2,11 @@ import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 
 import './body.html';
+
+Template.body.helpers({
+  coffees: [
+    { name: "Duncan Donuts Medium Roast" },
+    { name: "Kirkland 5000" },
+    { name: "Starbucks Pike Place Roast" },
+  ],
+});
