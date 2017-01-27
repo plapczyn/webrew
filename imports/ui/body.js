d6600113c2ee5a77432d4c685d53a11b75786117
Paul Lapczynski <lapczyn1@cse.msu.edu>
2017-01-26 22:25:08 -0500

    Test Commit

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -2,6 +2,7 @@ import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 
 import './body.html';
+import './templates/coffee.html';
 
 Template.body.helpers({
   coffees: [
