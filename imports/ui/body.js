5511eefc19a43d945b90ab7e1e13a66613a69cde
Paul Lapczynski <lapczyn1@cse.msu.edu>
2017-01-26 23:03:22 -0500

    Test Commit

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -6,8 +6,8 @@ import './templates/coffee.html';
 
 Template.body.helpers({
   coffees: [
-    { name: "Duncan Donuts Medium Roast" },
-    { name: "Kirkland 5000" },
-    { name: "Starbucks Pike Place Roast" },
+    { name: "Duncan Donuts", roast: "Medium Roast" },
+    { name: "Kirkland 5000", roast: "" },
+    { name: "Starbucks Pike Place", roast: "Medium Roast" },
   ],
 });
