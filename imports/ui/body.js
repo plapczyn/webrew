c7df2bbd53eed8dc6c9ed8912aef7fd5e1d19238
petelapczynski <petelapczynski@gmail.com>
2017-01-27 20:17:12 -0500

    added coffee

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -7,7 +7,8 @@ import './templates/coffee.html';
 Template.body.helpers({
   coffees: [
     { name: "Duncan Donuts", roast: "Medium Roast" },
-    { name: "Kirkland 5000", roast: "" },
+    { name: "Kirkland 5000", roast: "Medium Roast" },
     { name: "Starbucks Pike Place", roast: "Medium Roast" },
+    { name: "Tim Hortons", roast: "Dark Roast" },
   ],
 });
