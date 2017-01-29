39efb0da8748c2ce5942c8461d413af6557f16aa
paul-lapczynski <paul.lapczynski@gmail.com>
2017-01-29 14:59:15 -0500

    added favorites

--- imports/ui/templates/mebrew.js
+++ imports/ui/templates/mebrew.js
@@ -1,5 +1,6 @@
 import './mebrew.html';
 import './mebrew.css';
+import { Coffees } from '../../api/collections/coffees.js';
 
 Template.mebrew.onCreated(() =>{
   this.counter = 0;
@@ -15,8 +16,12 @@ Template.mebrew.events({
 
 Template.mebrew.helpers({
   name: "Paul",
-  pictureUrl:"img/Paul.jpg",
+  pictureUrl:"/img/Paul.jpg",
   coffeeReviews:[{name:"TEST"},{},{},{},{},{}],
   info: "This is some test information... I LOVE INFORMATION",
-  favorites: ["Kirkland", "Duncan Donuts", "Kirkland Jungle Blend"]
+  favorites: ["Kirkland", "Tyler's Treat"],
+
+  brew (element) {
+    return Coffees.find({name:element.hash.name});
+  }
 });
