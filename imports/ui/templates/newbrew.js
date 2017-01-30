6367091aefb9784d7fb6e4bcffb18db4488c2435
Philip Lapczynski <philip.lapczynski@gmail.com>
2017-01-30 02:04:18 -0500

    Cleaned up the brew view and newBrew pages. Added support for markdown flavor descriptions

--- imports/ui/templates/newbrew.js
+++ imports/ui/templates/newbrew.js
@@ -1,4 +1,5 @@
 import './newbrew.html';
+import './newbrew.css';
 
 import { Coffees } from '../../api/collections/coffees.js';
 
@@ -11,19 +12,19 @@ Template.newbrew.events({
     const target = event.target;
     const name = target.name.value;
     const roast = target.roast.value;
+    const description = target.description.value;
 
     // Insert a new coffee into the collection
     Coffees.insert({
       name: name,
       roast: roast,
+      description: description,
       createdAt: new Date(),
       owner: Meteor.userId(),
       username: Meteor.user().username,
     });
 
     // Clear form
-    target.name.value = '';
-    target.roast.value = '';
     FlowRouter.go('Main');
   },
 });
