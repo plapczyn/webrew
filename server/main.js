0cce41f60a4bc4a849fcfd2083ca6d0acdbb0a8a
paul-lapczynski <paul.lapczynski@gmail.com>
2017-02-11 09:27:26 -0500

    removed auto publish and added basic search functionality in the home page.

--- server/main.js
+++ server/main.js
@@ -1,7 +1,35 @@
 import { Meteor } from 'meteor/meteor';
-
-import '../imports/api/collections/coffees.js';
-
+import {Coffees } from '../imports/api/collections/coffees.js'
+import {Check} from 'meteor/check'
 Meteor.startup(() => {
   // code to run on server at startup
 });
+
+
+if (Meteor.isServer) {
+  console.log("ASDASD");
+  // This code only runs on the server
+  Meteor.publish('coffees', function( search ) {
+      check( search, Match.OneOf( String, null, undefined ) );
+  console.log(search);
+    let query      = {},
+        projection = { limit: 9, sort: { title: 0 } };
+
+    if ( search ) {
+      let regex = new RegExp( search, 'i' );
+
+      query = {
+        $or: [
+          { name: regex },
+        ]
+      };
+
+      projection.limit = 99;
+
+    return Coffees.find({name: {$regex: new RegExp(search, 'i')}});
+    }
+
+  return Coffees.find();
+  });
+
+}
