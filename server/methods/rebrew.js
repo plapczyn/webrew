b8badddd05253023c8f86d27a65bffa5f3370309
Philip Lapczynski <philip.lapczynski@gmail.com>
2017-02-19 14:39:39 -0500

    Moved calculation of average rating to the backend

--- server/methods/rebrew.js
+++ server/methods/rebrew.js
@@ -1,4 +1,5 @@
 import { Rebrews } from '../../imports/api/collections/coffees.js';
+import { Coffees } from '../../imports/api/collections/coffees.js';
 
 if(Meteor.isServer){
   Meteor.methods({
@@ -6,6 +7,11 @@ if(Meteor.isServer){
     ///{user: String, name: String}
     'rebrews.add'(rebrew){
       Rebrews.insert(rebrew);
+      let allreviews = Rebrews.find({brew:rebrew.brew}).fetch();
+      let ratings = _.pluck(allreviews, "rating");
+      let sum = ratings.reduce(function(a, b){return parseFloat(a) + parseFloat(b);});
+      let average = (sum / ratings.length).toFixed(1);
+      Coffees.update(Coffees.findOne({name:rebrew.brew})._id, {$set: {averageRating: average}});
     }
   });
 }
