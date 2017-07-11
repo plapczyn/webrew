9d629134f4019b51bd21a14c909d975fce5f6ebc
petelapczynski <petelapczynski@gmail.com>
2017-07-10 20:37:43 -0400

    use user photo and email for facebook-google service onCreateUser

--- server/main.js
+++ server/main.js
@@ -1,6 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import {Check} from 'meteor/check';
 import {Rebrews} from '../imports/api/collections/coffees.js';
+import {BrewFiles} from '../imports/api/collections/coffees.js';
 
 Meteor.startup(() => {
   // code to run on server at startup
@@ -30,18 +31,33 @@ Meteor.startup(() => {
 });
 
 Accounts.onCreateUser(function (options, user) {
+    let brewfile = {};
+    brewfile.email = "";
+    brewfile.imageurl = "";
+    
     if (user.services.facebook) {
         //FacebookUser
         user.username = user.services.facebook.name;
-        user.emails = user.services.facebook.email;
-        return user;
+        brewfile.email = user.services.facebook.email;
+        brewfile.imageurl = "https://graph.facebook.com/" + user.services.facebook.id + "/picture/?type=large";
     } else if (user.services.google) {
         //GoogleUser
         user.username = user.services.google.name;
-        user.emails = user.services.google.email;
-        return user;
+        brewfile.email = user.services.google.email;
+        brewfile.imageurl = user.services.google.picture;
     } else { 
         //StandardUser
-        return user;
     }
+    
+    //create BrewFile profile
+    try{
+        BrewFiles.insert({ Username: user.username, ImageUrl: brewfile.imageurl, Email: brewfile.email });
+    }
+    catch(e){
+        console.log("Error inserting BrewFile during onCreateUser");
+        console.log(user);
+        console.log(brewfile);
+    }
+    
+    return user;
 });
